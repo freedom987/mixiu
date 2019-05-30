fae04267d39a4e7403f059b163a6b85f79055bab
freedom <38273586+freedom987@users.noreply.github.com>
2019-05-30 13:07:05 +0800

    freedom
    完成添加，编辑。未解决的问题：
    1、多个用户登陆获取数据的问题

---
+++ precache-manifest.70cca4a7ef0ba8aefbba53c08f55d5f1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b8ae1d07f132c442f8439f0cb8029f3",
+    "url": "/img/404.6b8ae1d0.gif"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c43f32ceb381011efa8fedbf566da00e",
+    "url": "/img/logo.c43f32ce.png"
+  },
+  {
+    "revision": "967d83bae713bc594f38",
+    "url": "/css/chunk-vendors.af67b8ba.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "663de5489889eb6fadbdf439a9c4ab68",
+    "url": "/img/showcase.663de548.png"
+  },
+  {
+    "revision": "639dc7b783ca01ec9701dd62a9005ca1",
+    "url": "/img/bg.639dc7b7.jpg"
+  },
+  {
+    "revision": "e69ad9e5d73a0cc845c8",
+    "url": "/js/app.56ffba8b.js"
+  },
+  {
+    "revision": "967d83bae713bc594f38",
+    "url": "/js/chunk-vendors.06ead327.js"
+  },
+  {
+    "revision": "585eeae3ddb0839b3230d410bd0d05e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b8ae1d07f132c442f8439f0cb8029f3",
+    "url": "/img/404.gif"
+  },
+  {
+    "revision": "639dc7b783ca01ec9701dd62a9005ca1",
+    "url": "/img/bg.jpg"
+  },
+  {
+    "revision": "c43f32ceb381011efa8fedbf566da00e",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "663de5489889eb6fadbdf439a9c4ab68",
+    "url": "/img/showcase.png"
+  },
+  {
+    "revision": "c77a5e52e968ca286aaf52bb3b55f047",
+    "url": "/css/reset.css"
+  },
+  {
+    "revision": "e69ad9e5d73a0cc845c8",
+    "url": "/css/app.cab51c25.css"
+  }
+];
